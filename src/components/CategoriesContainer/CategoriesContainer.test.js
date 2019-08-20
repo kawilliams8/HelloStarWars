6deeeb74d8d8d364f682c765c585f96b517ce362
Katie Williams <kawilliams8@gmail.com>
2019-08-20 16:13:51 -0600

    Add enzyme adapter and basic tests for each Component

---
+++ src/components/CategoriesContainer/CategoriesContainer.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CategoriesContainer from './CategoriesContainer';
+
+describe('CategoriesContainer', () => {
+  it('', () => {
+
+  });
+});
---
+++ src/components/CategoryCard/CategoryCard.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CategoryCard from './CategoryCard';
+
+describe('CategoryCard', () => {
+  it('', () => {
+
+  });
+});
