d9ae53a87fecb13e531dce43c2c2c401ff6ba794
Katie Williams <kawilliams8@gmail.com>
2019-08-27 20:19:19 -0600

    Updates

---
+++ precache-manifest.955fb2385a5cffed27902a1d43e0d574.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6da055f76a0ba2d9248e7d0106c84e8",
+    "url": "/HelloStarWars/index.html"
+  },
+  {
+    "revision": "1cd500331fa8d4ab5f3a",
+    "url": "/HelloStarWars/static/css/main.e02c3836.chunk.css"
+  },
+  {
+    "revision": "b3a8a6abc31a45a45f3d",
+    "url": "/HelloStarWars/static/js/2.90159d41.chunk.js"
+  },
+  {
+    "revision": "1cd500331fa8d4ab5f3a",
+    "url": "/HelloStarWars/static/js/main.c049a3d4.chunk.js"
+  },
+  {
+    "revision": "7ce0f7e19043ed83d829",
+    "url": "/HelloStarWars/static/js/runtime~main.b56d52e9.js"
+  },
+  {
+    "revision": "f2540fbc5e9505bd784ca497d46c7db2",
+    "url": "/HelloStarWars/static/media/Bow.f2540fbc.png"
+  },
+  {
+    "revision": "d850816165e0b9056e3afdb1544480de",
+    "url": "/HelloStarWars/static/media/Chewbacca.d8508161.png"
+  },
+  {
+    "revision": "8fe72e1fd51330c645ef02bd310bf024",
+    "url": "/HelloStarWars/static/media/DeathStar.8fe72e1f.png"
+  },
+  {
+    "revision": "d3469967453f81e4fa6d14468b3dc3c1",
+    "url": "/HelloStarWars/static/media/Dunkin.d3469967.ttf"
+  },
+  {
+    "revision": "6c00b899e940ecb036fa3bef8091f750",
+    "url": "/HelloStarWars/static/media/Heart.6c00b899.png"
+  },
+  {
+    "revision": "ccc49bf0bfef9f609318ef81905646e9",
+    "url": "/HelloStarWars/static/media/Japan.ccc49bf0.ttf"
+  },
+  {
+    "revision": "0ff427b73cb342dd68ee25ae0e79a185",
+    "url": "/HelloStarWars/static/media/PrincessLeia.0ff427b7.png"
+  },
+  {
+    "revision": "3373df6b920abace68acc509a6de3dec",
+    "url": "/HelloStarWars/static/media/Star.3373df6b.png"
+  },
+  {
+    "revision": "5bd4866e71014f1c48055bcd247cf116",
+    "url": "/HelloStarWars/static/media/Yoda.5bd4866e.png"
+  },
+  {
+    "revision": "b07fc198bebc94c2e300f19265b01e14",
+    "url": "/HelloStarWars/static/media/galaxy.b07fc198.jpg"
+  }
+]);
